014bffb77986b1124dda3cb92c3024a43632425c
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-04 23:27:51 +0000

    update query using avg()

--- 3_queries_group-by/5_average_completion_time.sql
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -1,7 +1,7 @@
-SELECT students.name as student, sum(assignment_submissions.duration)/count(assignment_submissions.*) as average_assignment_duration
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
 FROM students
 JOIN assignment_submissions ON students.id = student_id
-WHERE students.end_date IS NULL
+WHERE end_date IS NULL
 GROUP BY student
 ORDER BY average_assignment_duration DESC;
 
