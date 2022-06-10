5c80c8437f58e8dae83fdfcf0fdb42805fbbe4cc
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-10 03:09:52 +0000

    sql join students & cohorts

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,13 +8,15 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT id, name, cohort_id
+SELECT students.id, students.name, cohorts.name
 FROM students
+JOIN cohorts ON cohorts.id = cohort_id
 LIMIT 5;
 `)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`)
-  })
+  console.log(res.rows)
+  // res.rows.forEach(user => {
+  //   console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohorts.name} cohort`)
+  // })
 })
 .catch(err => console.error('query error', err.stack));
