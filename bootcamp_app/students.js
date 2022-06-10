1b5760f0c39bfd28caf9e7e3315129faf05d8b00
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-10 02:52:53 +0000

    loop through query, log sentence

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,6 +13,8 @@ FROM students
 LIMIT 5;
 `)
 .then(res => {
-  console.log(res);
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`)
+  })
 })
 .catch(err => console.error('query error', err.stack));
